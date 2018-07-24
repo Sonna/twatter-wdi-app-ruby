a19b1c002492a6f9d143331c607f7f64cbe2e038
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-24 11:14:42 +1000

    Add Forms to like & retwat buttongs
    Add forms to generate supposed `like` and `retwat` resources within a
    given `twat` message, in order to help inform what resources will need
    to be created or known in order for it too work as expected (whilst
    heavily borrowing from the existing Twitter User interface).

--- main.rb
+++ main.rb
@@ -4,8 +4,25 @@ $LOAD_PATH.push ROOT
 require "db_config"
 require "sinatra/base"
 
+# require "models/user"
+#
+# enable :sessions
+#
+# helpers do
+#   def current_user
+#     User.find_by(id: session[:user_id])
+#   end
+#
+#   def logged_in?
+#     !!current_user
+#   end
+# end
+
 class TwatterApp < Sinatra::Base
   set(:views, File.join(ROOT, "/templates"))
 
   get("/") { erb :index }
+  # get("/users/:user") { erb :user }
+  # post("/likes") { redirect "/timeline?twat_id=#{params["twat_id"]}" }
+  # post("/retwats") { redirect "/timeline?twat_id=#{retwat.id}" }
 end

415ea0cb6891961db7b31e48feaaf79cc6becdea
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-26 09:28:24 +1000

    Add missing feature tests for hidden elements
    Add missing User feature tests to check for other remaining hidden
    elements for a given logged in User, cannot see a Direct message button
    to themselves and cannot see the retwat button, since an User cannot
    retwat their own twat.

---
+++ test/features/user_features/direct_message_other_users_test.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module UserFeatures
+  class DirectMessageOtherUsersTest < CapybaraTestCase
+    def setup
+      visit "/logout"
+
+      @anna = MockUser.new("anna@email.com", "Test User", "testusername",
+                           "password")
+      @messenger = MockUser.new("messenger@email.com", "Test messenger",
+                                "testmessenger", "password")
+      anna = User.create(@anna.to_h)
+      messenger = User.create(@messenger.to_h)
+
+      @anna_twat = Twat.create(message: "Anna twat message", user_id: anna.id)
+      @messenger_twat =
+        Twat.create(message: "messenger's content", user_id: messenger.id)
+    end
+
+    def teardown
+      cleanup_user_data(@anna)
+      cleanup_user_data(@messenger)
+      @anna_twat.destroy
+      @messenger_twat.destroy
+    end
+
+    def test_user_can_find_the_direct_message_button
+      login(@messenger.email, @messenger.password)
+      assert find("#twat-#{@anna_twat.id}", text: "Direct Message")
+    end
+
+    def test_user_cannot_find_the_direct_message_button_for_their_own_twats
+      login(@anna.email, @anna.password)
+      assert_raises(Capybara::ElementNotFound) do
+        find("#twat-#{@anna_twat.id}", text: "Direct Message")
+      end
+    end
+
+    protected
+
+    def cleanup_user_data(user)
+      attributes = user.to_h
+      attributes.delete(:password)
+      User.find_by(attributes).destroy
+    end
+  end
+end
---
+++ test/features/user_features/retwat_other_users_test.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module UserFeatures
+  class RetwatOtherUsersTest < CapybaraTestCase
+    def setup
+      visit "/logout"
+
+      @bob = MockUser.new("bob@email.com", "Test Bob", "testbob", "password")
+      @carol = MockUser.new("carol@email.com", "Test Carol", "testcarol",
+                            "password")
+      bob = User.create(@bob.to_h)
+      carol = User.create(@carol.to_h)
+
+      @bob_twat = Twat.create(message: "bob twat message", user_id: bob.id)
+      @carol_twat = Twat.create(message: "carol's content", user_id: carol.id)
+    end
+
+    def teardown
+      cleanup_user_data(@bob)
+      cleanup_user_data(@carol)
+      @bob_twat.destroy
+      @carol_twat.destroy
+    end
+
+    def test_user_can_find_the_retwat_message_button
+      login(@carol.email, @carol.password)
+      assert find("#twat-#{@bob_twat.id}", text: "Retwat")
+    end
+
+    def test_user_cannot_find_the_retwat_message_button_for_their_own_twats
+      login(@bob.email, @bob.password)
+      assert_raises(Capybara::ElementNotFound) do
+        find("#twat-#{@bob_twat.id}", text: "Retwat")
+      end
+    end
+
+    protected
+
+    def cleanup_user_data(user)
+      attributes = user.to_h
+      attributes.delete(:password)
+      User.find_by(attributes).destroy
+    end
+  end
+end

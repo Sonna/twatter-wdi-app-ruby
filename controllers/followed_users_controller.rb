61375c9192bcb7d3c58488e4643e3c9900e1fbc6
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-27 09:22:17 +1000

    Add Follow / Unfollow button for Users
    Add a Follow / Unfollow button to the User profile card to allow the
    current User to preform that action, in order to add new Users to their
    own feed (like those coming in from Retwats).

--- controllers/followed_users_controller.rb
+++ controllers/followed_users_controller.rb
@@ -4,17 +4,17 @@ require "controllers/application_controller"
 require "models/followed_user"
 
 class FollowedUsersController < ApplicationController
-  # post "/follows" do
-  #   authorized?
-  #   FollowedUser.create(follower_id: current_user.id,
-  #                       user_id: params[:follower_id])
-  #   redirect to("/")
-  # end
+  post "/follows" do
+    authorized?
+    FollowedUser.create(follower_id: current_user.id,
+                        user_id: params[:follower_id])
+    redirect to("/")
+  end
 
-  # delete "/follows/:id" do
-  #   authorized?
-  #   FollowedUser.find_by(id: params[:id],
-  #                        follower_id: current_user.id)&.destroy
-  #   redirect to("/")
-  # end
+  delete "/follows/:id" do
+    authorized?
+    FollowedUser.find_by(id: params[:id],
+                         follower_id: current_user.id)&.destroy
+    redirect to("/")
+  end
 end

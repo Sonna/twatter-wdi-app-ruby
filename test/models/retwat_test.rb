5f5a9a52fabe5e7945ecf336789f9b804eee1e79
Alex Sonneveld <Alex.Sonneveld@live.com.au>
2018-07-26 22:34:03 +1000

    Add some model tests for Retwat
    Add some model tests that check a Retwat can be made and increases the
    count of retwats for a given Twat record.

---
+++ test/models/retwat_test.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "test/test_helper"
+
+module Models
+  class RetwatTest < Minitest::Test
+    def setup
+      @ronald = User.create(email: "ronald@email.com", name: "Test ronald",
+                            username: "testronald", password: "password")
+      @susan = User.create(email: "susan@email.com", name: "Test susan",
+                           username: "testsusan", password: "password")
+    end
+
+    def teardown
+      @ronald&.destroy
+      @susan&.destroy
+    end
+
+    def test_susan_can_retwats_ronalds_twat
+      subject = Twat.create(message: "ronald's eat shoots' leaves",
+                            user_id: @ronald.id)
+
+      assert described_class.create(twat_id: subject.id, user_id: @susan.id)
+    ensure
+      subject&.destroy
+    end
+
+    def test_ronald_twats_retwat_count_increases
+      subject = Twat.create(message: "ronald's eat shoots' leaves",
+                            user_id: @ronald.id)
+      assert_equal 0, subject.retwats_count
+
+      described_class.create(twat_id: subject.id, user_id: @susan.id)
+      assert_equal 1, subject.reload.retwats_count
+    ensure
+      subject&.destroy
+    end
+
+    protected
+
+    def described_class
+      Retwat
+    end
+  end
+end
